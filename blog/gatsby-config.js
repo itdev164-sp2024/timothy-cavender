b4e68e3e2325ac46950837c14fc3122df6ee4f54
unknown <cavendert@gmail.com>
2024-01-25 13:20:46 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `TJ Cavender`,
+      company: `MATC`,
+      address: `Milwaukee WI`
+    },
   },
   plugins: [
     `gatsby-plugin-image`,
